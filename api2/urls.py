859cab77e2047bf18baf6c9e5c6035c7db3c60fb
april <sue5520607@skuniv.ac.kr>
2023-08-05 23:43:12 +0900

    먕

--- api2/urls.py
+++ api2/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from api2.views import UserViewSet, PostViewSet
+from api2.views import UserViewSet, PostViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'Post', PostViewSet)
+router.register(r'post', PostViewSet)
+router.register(r'comment', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
